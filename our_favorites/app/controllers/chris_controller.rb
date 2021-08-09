4686f475e7dabbf2d5aa0a4d59a5ff99ef92dfa4
Chrisluna10 <chrisluna4224@gmail.com>
2021-08-09 16:18:01 -0700

    changes to files

--- our_favorites/app/controllers/chris_controller.rb
+++ our_favorites/app/controllers/chris_controller.rb
@@ -1,2 +1,11 @@
 class ChrisController < ApplicationController
+    def in_n_out
+        render 'in_n_out.html.erb'
+    end
+    def pizza_port
+        render 'pizza_port.html.erb'
+    end
+    def sushi_house
+        render 'sushi_house.html.erb'
+    end
 end
--- our_favorites/app/controllers/conrad_controller.rb
+++ our_favorites/app/controllers/conrad_controller.rb
@@ -1,2 +1,11 @@
 class ConradController < ApplicationController
+    def the_olive_garden
+        render 'the_olive_garden.html.erb'
+    end
+    def cheesecake_factory
+        render 'cheesecake_factory.html.erb'
+    end
+    def buffalo_wild_wings
+        render 'buffalo_wild_wings.html.erb'
+    end
 end
