fca0bf5791d6c52e87577ee6a9a23f4e762c9bf2
charper95 <charper1995@gmail.com>
2021-08-10 11:51:10 -0700

    finished some challenges

--- rails_params_c_b_r/app/controllers/main_controller.rb
+++ rails_params_c_b_r/app/controllers/main_controller.rb
@@ -1,8 +1,20 @@
 class MainController < ApplicationController
     def cubed
+         @cubed = params[:number].to_i ** 3
          render 'cubed.html.erb'
-         @cubed = params[:number].to_i
-         @cubed = :number**3
     end
-
+    def evenly
+        if params[:number1].to_i % params[:number2].to_i == 0
+            @result_string = "#{params[:number1]} is evenly divisible by #{params[:number2]}"
+        else
+            @result_string = "#{params[:number1]} is not evenly divisible by #{params[:number2]}"
+        end
+    end
+    def length
+        @length = params[:string].length
+    end
+    def palindrome
+    end
+    def madlib
+    end
 end
--- rails_params_c_b_r/config/routes.rb
+++ rails_params_c_b_r/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get '/:number' => 'main#cubed'
+  get 'evenly/:number1/:number2' => 'main#evenly'
+  get 'length/:string' => 'main#length'
 end
