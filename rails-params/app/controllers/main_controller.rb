7556e6c60e3c441c3eb80eae263e15ff5f95768f
galadoe <kgaladoe@gmail.com>
2021-08-10 11:57:51 -0700

    second challenge question completed

--- rails-params/app/controllers/main_controller.rb
+++ rails-params/app/controllers/main_controller.rb
@@ -1,8 +1,19 @@
 class MainController < ApplicationController
   def cube
     @number = params[:number].to_i
-    puts @number 
+    puts @number
     puts @number.class
     @cubed_number = @number**3
+  end
+
+  def evenly_divisble
+    @num1 = params[:num1].to_i
+    @num2 = params[:num2].to_i
+    @divisble = @num1 % @num2
+      if @divisble % @num1 === 0
+        @divisble = true
+      else
+        @divisble = false
+      end
   end
 end
--- rails-params/config/routes.rb
+++ rails-params/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/cubed/:number' => 'main#cube'
+
+  get '/evenly_divisble/:num1/:num2' => 'main#evenly_divisble' 
 end
